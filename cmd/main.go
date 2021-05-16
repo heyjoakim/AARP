0d37f7b58175962fa03946387aca17a1865fdd61
heyjoakim <joakimhey@gmail.com>
2021-05-16 15:34:04 +0200

    better structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
+	"os"
+
 	"github.com/heyjoakim/AARP/data"
 	"github.com/heyjoakim/AARP/helpers"
 	"github.com/heyjoakim/AARP/logic"
 	"github.com/heyjoakim/AARP/set"
 	log "github.com/sirupsen/logrus"
-	"os"
 )
 
 func main() {
 	root := "/home/hey/git/Zeeguu-API"
-
 	extension := ".py"
 	exclusion := "__init__.py"
-	fileSlice := logic.WalkDirs(root, extension, exclusion)
-	helpers.CountFiles(fileSlice)
+
+	files := logic.WalkDirs(root, extension, exclusion)
+	helpers.CountFiles(files)
 
 	f, err := os.Create("data.wsd")
 	if err != nil {
 		log.Fatal(err)
 	}
 	f.WriteString("@startuml\ntitle Automated Diagram\nskinparam nodesep 100\nskinparam ranksep 100\n")
+
 	dependencies := make(map[string]bool)
 	nodes := make(map[string]bool)
 
-	for _, j := range *fileSlice {
-		file, imports := helpers.ReadLines(j)
-		dependencies, nodes = set.WriteToSet(dependencies, nodes, file, imports)
+	for _, j := range *files {
+		f, imports := logic.ReadLines(j)
+		dependencies, nodes = set.WriteToSet(dependencies, nodes, f, imports)
 	}
 	data.WriteFile(f, nodes)
 	data.WriteFile(f, dependencies)
